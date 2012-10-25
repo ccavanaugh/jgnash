dfb882ef53f793afa0216b8e27f5370ff4207379
Craig Cavanaugh <jgnash.devel@gmail.com>
2012-10-25 05:53:09 -0400

    Additional improvements to establishing a default paper size and margin for reports.

--- jgnash-swing/src/main/java/jgnash/ui/report/ReportPrintFactory.java
+++ jgnash-swing/src/main/java/jgnash/ui/report/ReportPrintFactory.java
@@ -20,15 +20,20 @@ package jgnash.ui.report;
 import java.awt.print.PageFormat;
 import java.awt.print.Paper;
 import java.awt.print.PrinterJob;
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.prefs.Preferences;
+
+import javax.print.PrintService;
+import javax.print.attribute.standard.MediaSize;
 
 import jgnash.ui.report.jasper.DynamicJasperReport;
 
 /**
  * Factory class for handling printing preferences
- *
+ * 
  * @author Craig Cavanaugh
- *
+ * 
  */
 public class ReportPrintFactory {
     private static final String HEIGHT = "height";
@@ -39,24 +44,51 @@ public class ReportPrintFactory {
     private static final String IMAGEABLE_X = "imageableX";
     private static final String IMAGEABLE_Y = "imageableY";
 
+    private static final int DEFAULT_MARGIN = 43; // ~.60" margin
+
     private ReportPrintFactory() {
     }
 
     /**
-     * Gets the default page format which is letter size
-     *
+     * Returns the default paper size for a report
+     * 
      * @return page format
      */
-    private static PageFormat getDefaultPage() {          
+    private static PageFormat getDefaultPage() {
+
+        /* A4 is the assumed default */
+        MediaSize defaultMediaSize = MediaSize.ISO.A4;
+
+        /* US and Canada use letter size as the default */
+        Locale[] letterLocales = new Locale[] { Locale.US, Locale.CANADA, Locale.CANADA_FRENCH };
+        if (Arrays.asList(letterLocales).contains(Locale.getDefault())) {
+            defaultMediaSize = MediaSize.NA.LETTER;
+        }
+
+        /* Create the default paper size with a default margin */
+        Paper paper = new Paper();
+
+        int width = (int) (defaultMediaSize.getX(MediaSize.INCH) / (1f / 72f));
+        int height = (int) (defaultMediaSize.getY(MediaSize.INCH) / (1f / 72f));       
+
+        paper.setSize(width, height);
+        paper.setImageableArea(DEFAULT_MARGIN, DEFAULT_MARGIN, width - (2 * DEFAULT_MARGIN), height - (2 * DEFAULT_MARGIN));
+
         PageFormat format = new PageFormat();
-        format.setPaper(new Paper());
-        
-        return format;                               
+        format.setPaper(paper);
+
+        /* if a default printer is found, validate the page */
+        PrintService[] services = PrinterJob.lookupPrintServices();
+        if (services.length != 0) { // no printers found on the system.                     
+            format = PrinterJob.getPrinterJob().validatePage(format);
+        }
+
+        return format;
     }
 
     /**
      * Save a <code>PageFormat</code> to preferences
-     *
+     * 
      * @param report report
      * @param format <code>PageFormat</code> to save
      */
@@ -73,7 +105,7 @@ public class ReportPrintFactory {
         p.putDouble(IMAGEABLE_HEIGHT, paper.getImageableHeight());
         p.putDouble(IMAGEABLE_WIDTH, paper.getImageableWidth());
         p.putDouble(IMAGEABLE_X, paper.getImageableX());
-        p.putDouble(IMAGEABLE_Y, paper.getImageableY());            
+        p.putDouble(IMAGEABLE_Y, paper.getImageableY());
     }
 
     public static PageFormat getPageFormat(final DynamicJasperReport report) {
