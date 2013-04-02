ab4cc045028c85765769823888774cfce2808ae8
Craig Cavanaugh <jgnash.devel@gmail.com>
2013-04-02 06:40:19 -0400

    Add missing file

---
+++ jgnash-core/src/main/java/jgnash/message/EncryptionFilter.java
@@ -0,0 +1,97 @@
+/*
+ * jGnash, a personal finance application
+ * Copyright (C) 2001-2013 Craig Cavanaugh
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package jgnash.message;
+
+import org.apache.commons.codec.binary.Base64;
+
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * A Simple encryption filter based on a supplied user and password
+ *
+ * @author Craig Cavanaugh
+ */
+public class EncryptionFilter {
+
+    private Key key;
+
+    private static final String ENCRYPTION_ALGORITHM = "AES";
+
+    private static Logger logger = Logger.getLogger(EncryptionFilter.class.getName());
+
+    public EncryptionFilter(final String user, final char[] password) {
+        byte[] encryptionKey = "fake".getBytes();
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            StringBuilder builder = new StringBuilder(user);
+            builder.append(password);
+
+            // generate the encryption key
+            encryptionKey = md.digest(builder.toString().getBytes());
+
+            builder.delete(0, builder.length() - 1);
+        } catch (NoSuchAlgorithmException e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+        }
+
+        key = new SecretKeySpec(encryptionKey, ENCRYPTION_ALGORITHM);
+    }
+
+    public String encrypt(final String plain) {
+
+        try {
+            Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+
+            return Base64.encodeBase64String(cipher.doFinal(plain.getBytes()));
+
+        } catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | BadPaddingException | IllegalBlockSizeException e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+        }
+
+        return null;
+    }
+
+    public String decrypt(final String encrypted) {
+
+        try {
+            Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, key);
+
+            return new String(cipher.doFinal(Base64.decodeBase64(encrypted)));
+        } catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | BadPaddingException | IllegalBlockSizeException e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+        }
+
+        return null;
+    }
+}
