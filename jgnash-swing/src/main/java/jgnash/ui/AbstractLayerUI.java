859115188c2f7873d743edcb0ca243f29b27b252
Craig Cavanaugh <jgnash.devel@gmail.com>
2013-05-16 04:46:13 -0400

    Fix javadoc

--- jgnash-swing/src/main/java/jgnash/ui/AbstractLayerUI.java
+++ jgnash-swing/src/main/java/jgnash/ui/AbstractLayerUI.java
@@ -52,327 +52,298 @@ import java.beans.PropertyChangeEvent;
  * abstract methods in the {@link LayerUI} class. It takes care of the
  * management of {@code LayerItemListener}s and defines the hook method to
  * configure the {@code Graphics2D} instance specified in the
- * {@link #paint(Graphics,JComponent)} method. It also provides convenient
+ * {@link #paint(Graphics, JComponent)} method. It also provides convenient
  * methods named {@code process<eventType>Event} to process the given class of
  * event.
  * <p/>
  * If state of the {@code AbstractLayerUI} is changed, call
  * {@link #setDirty(boolean)} with {@code true} as the parameter, it will
  * repaint all {@code JXLayer}s connected with this {@code AbstractLayerUI}
- * 
+ *
  * @see JLayer#setUI(LayerUI)
  */
 public class AbstractLayerUI<V extends JComponent> extends LayerUI<V> {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private static final Map<RenderingHints.Key, Object> emptyRenderingHintMap = Collections
-			.unmodifiableMap(new HashMap<RenderingHints.Key, Object>(0));
+    private static final Map<RenderingHints.Key, Object> emptyRenderingHintMap = Collections
+            .unmodifiableMap(new HashMap<RenderingHints.Key, Object>(0));
 
-	private boolean isDirty;
+    private boolean isDirty;
 
-	@Override
+    @Override
     @SuppressWarnings("rawtypes")
-	public void installUI(JComponent c) {
-		super.installUI(c);
-		((JLayer) c).setLayerEventMask(getLayerEventMask());
-	}
+    public void installUI(JComponent c) {
+        super.installUI(c);
+        ((JLayer) c).setLayerEventMask(getLayerEventMask());
+    }
 
-	@Override
+    @Override
     @SuppressWarnings("rawtypes")
-	public void uninstallUI(JComponent c) {
-		super.uninstallUI(c);
-		((JLayer) c).setLayerEventMask(0);
-	}
+    public void uninstallUI(JComponent c) {
+        super.uninstallUI(c);
+        ((JLayer) c).setLayerEventMask(0);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public void handlePropertyChangeEvent(PropertyChangeEvent evt,
-			JLayer<? extends V> l) {
-		if (!"dirty".equals(evt.getPropertyName())
-				|| evt.getNewValue() == Boolean.TRUE) {
-			l.repaint();
-		}
-	}
+    public void handlePropertyChangeEvent(PropertyChangeEvent evt, JLayer<? extends V> l) {
+        if (!"dirty".equals(evt.getPropertyName()) || evt.getNewValue() == Boolean.TRUE) {
+            l.repaint();
+        }
+    }
 
-	/**
-	 * Returns the "dirty bit". If {@code true}, then the
-	 * {@code AbstractLayerUI} is considered dirty and in need of being
-	 * repainted.
-	 * 
-	 * @return {@code true} if the {@code AbstractLayerUI} state has changed and
-	 *         the {@link JLayer}s it is set to need to be repainted.
-	 */
-	protected boolean isDirty() {
-		return isDirty;
-	}
+    /**
+     * Returns the "dirty bit". If {@code true}, then the
+     * {@code AbstractLayerUI} is considered dirty and in need of being
+     * repainted.
+     *
+     * @return {@code true} if the {@code AbstractLayerUI} state has changed and
+     *         the {@link JLayer}s it is set to need to be repainted.
+     */
+    protected boolean isDirty() {
+        return isDirty;
+    }
 
-	/**
-	 * Sets the "dirty bit". If {@code isDirty} is {@code true}, then the
-	 * {@code AbstractLayerUI} is considered dirty and it triggers the
-	 * repainting of the {@link JLayer}s this {@code AbstractLayerUI} it is set
-	 * to.
-	 * 
-	 * @param isDirty
-	 *            whether this {@code AbstractLayerUI} is dirty or not.
-	 */
+    /**
+     * Sets the "dirty bit". If {@code isDirty} is {@code true}, then the
+     * {@code AbstractLayerUI} is considered dirty and it triggers the
+     * repainting of the {@link JLayer}s this {@code AbstractLayerUI} it is set
+     * to.
+     *
+     * @param isDirty whether this {@code AbstractLayerUI} is dirty or not.
+     */
     void setDirty(boolean isDirty) {
-		boolean oldDirty = this.isDirty;
-		this.isDirty = isDirty;
-		firePropertyChange("dirty", oldDirty, isDirty);
-	}
+        boolean oldDirty = this.isDirty;
+        this.isDirty = isDirty;
+        firePropertyChange("dirty", oldDirty, isDirty);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 * <p/>
-	 * <b>Note:</b> It is rarely necessary to override this method, for custom
-	 * painting override {@link #paintLayer(Graphics2D,JLayer)} instead
-	 * <p/>
-	 * This method configures the passed {@code Graphics} with help of the
-	 * {@link #configureGraphics(Graphics2D,JLayer)} method, then calls
-	 * {@code paintLayer(Graphics2D,JXLayer)} and resets the "dirty bit" at the
-	 * end.
-	 * 
-	 * @see #configureGraphics(Graphics2D,JLayer)
-	 * @see #paintLayer(Graphics2D,JLayer)
-	 * @see #setDirty(boolean)
-	 */
-	@Override
+    /**
+     * {@inheritDoc}
+     * <p/>
+     * <b>Note:</b> It is rarely necessary to override this method, for custom
+     * painting override {@link #paintLayer(Graphics2D, JLayer)} instead
+     * <p/>
+     * This method configures the passed {@code Graphics} with help of the
+     * {@link #configureGraphics(Graphics2D, JLayer)} method, then calls
+     * {@code paintLayer(Graphics2D, JXLayer)} and resets the "dirty bit" at the
+     * end.
+     *
+     * @see #configureGraphics(Graphics2D, JLayer)
+     * @see #paintLayer(Graphics2D, JLayer)
+     * @see #setDirty(boolean)
+     */
+    @Override
     @SuppressWarnings("unchecked")
-	public void paint(Graphics g, JComponent c) {
-		if (g instanceof Graphics2D) {
-			Graphics2D g2 = (Graphics2D) g.create();
-			JLayer<V> l = (JLayer<V>) c;
-			configureGraphics(g2, l);
-			paintLayer(g2, l);
-			g2.dispose();
-			setDirty(false);
-		}
-	}
+    public void paint(Graphics g, JComponent c) {
+        if (g instanceof Graphics2D) {
+            Graphics2D g2 = (Graphics2D) g.create();
+            JLayer<V> l = (JLayer<V>) c;
+            configureGraphics(g2, l);
+            paintLayer(g2, l);
+            g2.dispose();
+            setDirty(false);
+        }
+    }
 
-	/**
-	 * Subclasses should implement this method and perform custom painting
-	 * operations here.
-	 * <p/>
-	 * The default implementation paints the passed {@code JXLayer} as is.
-	 * 
-	 * @param g2
-	 *            the {@code Graphics2D} context in which to paint
-	 * @param l
-	 *            the {@code JXLayer} being painted
-	 */
-	protected void paintLayer(Graphics2D g2, JLayer<? extends V> l) {
-		l.paint(g2);
-	}
+    /**
+     * Subclasses should implement this method and perform custom painting
+     * operations here.
+     * <p/>
+     * The default implementation paints the passed {@code JXLayer} as is.
+     *
+     * @param g2 the {@code Graphics2D} context in which to paint
+     * @param l  the {@code JXLayer} being painted
+     */
+    protected void paintLayer(Graphics2D g2, JLayer<? extends V> l) {
+        l.paint(g2);
+    }
 
-	/**
-	 * This method is called by the {@link #paint} method prior to any drawing
-	 * operations to configure the {@code Graphics2D} object. The default
-	 * implementation sets the {@link Composite}, the clip,
-	 * {@link AffineTransform} and rendering hints obtained from the
-	 * corresponding hook methods.
-	 * 
-	 * @param g2
-	 *            the {@code Graphics2D} object to configure
-	 * @param l
-	 *            the {@code JXLayer} being painted
-	 * 
-	 * @see #getComposite(JLayer)
-	 * @see #getClip(JLayer)
-	 * @see #getTransform(JLayer)
-	 * @see #getRenderingHints(JLayer)
-	 */
+    /**
+     * This method is called by the {@link #paint} method prior to any drawing
+     * operations to configure the {@code Graphics2D} object. The default
+     * implementation sets the {@link Composite}, the clip,
+     * {@link AffineTransform} and rendering hints obtained from the
+     * corresponding hook methods.
+     *
+     * @param g2 the {@code Graphics2D} object to configure
+     * @param l  the {@code JXLayer} being painted
+     * @see #getComposite(JLayer)
+     * @see #getClip(JLayer)
+     * @see #getTransform(JLayer)
+     * @see #getRenderingHints(JLayer)
+     */
     void configureGraphics(Graphics2D g2, JLayer<? extends V> l) {
-		Composite composite = getComposite(l);
-		if (composite != null) {
-			g2.setComposite(composite);
-		}
-		Shape clip = getClip(l);
-		if (clip != null) {
-			g2.clip(clip);
-		}
-		AffineTransform transform = getTransform(l);
-		if (transform != null) {
-			g2.transform(transform);
-		}
-		Map<RenderingHints.Key, Object> hints = getRenderingHints(l);
-		if (hints != null) {
-			for (RenderingHints.Key key : hints.keySet()) {
-				Object value = hints.get(key);
-				if (value != null) {
-					g2.setRenderingHint(key, hints.get(key));
-				}
-			}
-		}
-	}
+        Composite composite = getComposite(l);
+        if (composite != null) {
+            g2.setComposite(composite);
+        }
+        Shape clip = getClip(l);
+        if (clip != null) {
+            g2.clip(clip);
+        }
+        AffineTransform transform = getTransform(l);
+        if (transform != null) {
+            g2.transform(transform);
+        }
+        Map<RenderingHints.Key, Object> hints = getRenderingHints(l);
+        if (hints != null) {
+            for (RenderingHints.Key key : hints.keySet()) {
+                Object value = hints.get(key);
+                if (value != null) {
+                    g2.setRenderingHint(key, hints.get(key));
+                }
+            }
+        }
+    }
 
-	/**
-	 * Returns the {@link Composite} to be used during painting of this
-	 * {@code JXLayer}, the default implementation returns {@code null}.
-	 * 
-	 * @param l
-	 *            the {@code JXLayer} being painted
-	 * 
-	 * @return the {@link Composite} to be used during painting for the
-	 *         {@code JXLayer}
-	 */
+    /**
+     * Returns the {@link Composite} to be used during painting of this
+     * {@code JXLayer}, the default implementation returns {@code null}.
+     *
+     * @param l the {@code JXLayer} being painted
+     * @return the {@link Composite} to be used during painting for the
+     *         {@code JXLayer}
+     */
     Composite getComposite(JLayer<? extends V> l) {
-		return null;
-	}
+        return null;
+    }
 
-	/**
-	 * Returns the {@link AffineTransform} to be used during painting of this
-	 * {@code JXLayer}, the default implementation returns {@code null}.
-	 * 
-	 * @param l
-	 *            the {@code JXLayer} being painted
-	 * 
-	 * @return the {@link AffineTransform} to be used during painting of the
-	 *         {@code JXLayer}
-	 */
+    /**
+     * Returns the {@link AffineTransform} to be used during painting of this
+     * {@code JXLayer}, the default implementation returns {@code null}.
+     *
+     * @param l the {@code JXLayer} being painted
+     * @return the {@link AffineTransform} to be used during painting of the
+     *         {@code JXLayer}
+     */
     AffineTransform getTransform(JLayer<? extends V> l) {
-		return null;
-	}
+        return null;
+    }
 
-	/**
-	 * Returns the {@link Shape} to be used as the clip during painting of this
-	 * {@code JXLayer}, the default implementation returns {@code null}.
-	 * 
-	 * @param l
-	 *            the {@code JXLayer} being painted
-	 * 
-	 * @return the {@link Shape} to be used as the clip during painting of the
-	 *         {@code JXLayer}
-	 */
+    /**
+     * Returns the {@link Shape} to be used as the clip during painting of this
+     * {@code JXLayer}, the default implementation returns {@code null}.
+     *
+     * @param l the {@code JXLayer} being painted
+     * @return the {@link Shape} to be used as the clip during painting of the
+     *         {@code JXLayer}
+     */
     Shape getClip(JLayer<? extends V> l) {
-		return null;
-	}
+        return null;
+    }
 
-	/**
-	 * Returns the map of rendering hints to be used during painting of this
-	 * {@code JXLayer}, the default implementation returns the empty
-	 * unmodifiable map.
-	 * 
-	 * @param l
-	 *            the {@code JXLayer} being painted
-	 * 
-	 * @return the map of rendering hints to be used during painting of the
-	 *         {@code JXLayer}
-	 */
+    /**
+     * Returns the map of rendering hints to be used during painting of this
+     * {@code JXLayer}, the default implementation returns the empty
+     * unmodifiable map.
+     *
+     * @param l the {@code JXLayer} being painted
+     * @return the map of rendering hints to be used during painting of the
+     *         {@code JXLayer}
+     */
     Map<RenderingHints.Key, Object> getRenderingHints(
             JLayer<? extends V> l) {
-		return emptyRenderingHintMap;
-	}
+        return emptyRenderingHintMap;
+    }
 
-	/**
-	 * By default only mouse, mouse motion, mouse wheel, keyboard and focus
-	 * events are supported, if you need to catch any other type of events,
-	 * override this method to return the different mask
-	 * 
-	 * @see JLayer#setLayerEventMask(long)
-	 */
+    /**
+     * By default only mouse, mouse motion, mouse wheel, keyboard and focus
+     * events are supported, if you need to catch any other type of events,
+     * override this method to return the different mask
+     *
+     * @see JLayer#setLayerEventMask(long)
+     */
     long getLayerEventMask() {
-		return AWTEvent.MOUSE_EVENT_MASK | AWTEvent.MOUSE_MOTION_EVENT_MASK
-				| AWTEvent.MOUSE_WHEEL_EVENT_MASK | AWTEvent.KEY_EVENT_MASK
-				| AWTEvent.FOCUS_EVENT_MASK;
-	}
+        return AWTEvent.MOUSE_EVENT_MASK | AWTEvent.MOUSE_MOTION_EVENT_MASK
+                | AWTEvent.MOUSE_WHEEL_EVENT_MASK | AWTEvent.KEY_EVENT_MASK
+                | AWTEvent.FOCUS_EVENT_MASK;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 * <p/>
-	 * This method calls the appropriate {@code process<eventType>Event} method
-	 * for the given class of event.
-	 */
-	@Override
-	public void eventDispatched(AWTEvent e, JLayer<? extends V> l) {
-		if (e instanceof FocusEvent) {
-			processFocusEvent((FocusEvent) e, l);
-		} else if (e instanceof MouseEvent) {
-			switch (e.getID()) {
-			case MouseEvent.MOUSE_PRESSED:
-			case MouseEvent.MOUSE_RELEASED:
-			case MouseEvent.MOUSE_CLICKED:
-			case MouseEvent.MOUSE_ENTERED:
-			case MouseEvent.MOUSE_EXITED:
-				processMouseEvent((MouseEvent) e, l);
-				break;
-			case MouseEvent.MOUSE_MOVED:
-			case MouseEvent.MOUSE_DRAGGED:
-				processMouseMotionEvent((MouseEvent) e, l);
-				break;
-			case MouseEvent.MOUSE_WHEEL:
-				processMouseWheelEvent((MouseWheelEvent) e, l);
-				break;
-			}
-		} else if (e instanceof KeyEvent) {
-			processKeyEvent((KeyEvent) e, l);
-		}
-	}
+    /**
+     * {@inheritDoc}
+     * <p/>
+     * This method calls the appropriate {@code process<eventType>Event} method
+     * for the given class of event.
+     */
+    @Override
+    public void eventDispatched(AWTEvent e, JLayer<? extends V> l) {
+        if (e instanceof FocusEvent) {
+            processFocusEvent((FocusEvent) e, l);
+        } else if (e instanceof MouseEvent) {
+            switch (e.getID()) {
+                case MouseEvent.MOUSE_PRESSED:
+                case MouseEvent.MOUSE_RELEASED:
+                case MouseEvent.MOUSE_CLICKED:
+                case MouseEvent.MOUSE_ENTERED:
+                case MouseEvent.MOUSE_EXITED:
+                    processMouseEvent((MouseEvent) e, l);
+                    break;
+                case MouseEvent.MOUSE_MOVED:
+                case MouseEvent.MOUSE_DRAGGED:
+                    processMouseMotionEvent((MouseEvent) e, l);
+                    break;
+                case MouseEvent.MOUSE_WHEEL:
+                    processMouseWheelEvent((MouseWheelEvent) e, l);
+                    break;
+            }
+        } else if (e instanceof KeyEvent) {
+            processKeyEvent((KeyEvent) e, l);
+        }
+    }
 
-	/**
-	 * Processes {@code FocusEvent} occurring on the {@link JLayer} or any of
-	 * its subcomponents.
-	 * 
-	 * @param e
-	 *            the {@code FocusEvent} to be processed
-	 * @param l
-	 *            the layer this LayerUI is set to
-	 */
-	@Override
+    /**
+     * Processes {@code FocusEvent} occurring on the {@link JLayer} or any of
+     * its subcomponents.
+     *
+     * @param e the {@code FocusEvent} to be processed
+     * @param l the layer this LayerUI is set to
+     */
+    @Override
     protected void processFocusEvent(FocusEvent e, JLayer<? extends V> l) {
-	}
+    }
 
-	/**
-	 * Processes {@code MouseEvent} occurring on the {@link JLayer} or any of
-	 * its subcomponents.
-	 * 
-	 * @param e
-	 *            the {@code MouseEvent} to be processed
-	 * @param l
-	 *            the layer this LayerUI is set to
-	 */
-	@Override
+    /**
+     * Processes {@code MouseEvent} occurring on the {@link JLayer} or any of
+     * its subcomponents.
+     *
+     * @param e the {@code MouseEvent} to be processed
+     * @param l the layer this LayerUI is set to
+     */
+    @Override
     protected void processMouseEvent(MouseEvent e, JLayer<? extends V> l) {
-	}
+    }
 
-	/**
-	 * Processes mouse motion events occurring on the {@link JLayer} or any of
-	 * its subcomponents.
-	 * 
-	 * @param e
-	 *            the {@code MouseEvent} to be processed
-	 * @param l
-	 *            the layer this LayerUI is set to
-	 */
-	@Override
+    /**
+     * Processes mouse motion events occurring on the {@link JLayer} or any of
+     * its subcomponents.
+     *
+     * @param e the {@code MouseEvent} to be processed
+     * @param l the layer this LayerUI is set to
+     */
+    @Override
     protected void processMouseMotionEvent(MouseEvent e, JLayer<? extends V> l) {
-	}
+    }
 
-	/**
-	 * Processes {@code MouseWheelEvent} occurring on the {@link JLayer} or any
-	 * of its subcomponents.
-	 * 
-	 * @param e
-	 *            the {@code MouseWheelEvent} to be processed
-	 * @param l
-	 *            the layer this LayerUI is set to
-	 */
-	@Override
+    /**
+     * Processes {@code MouseWheelEvent} occurring on the {@link JLayer} or any
+     * of its subcomponents.
+     *
+     * @param e the {@code MouseWheelEvent} to be processed
+     * @param l the layer this LayerUI is set to
+     */
+    @Override
     protected void processMouseWheelEvent(MouseWheelEvent e,
-			JLayer<? extends V> l) {
-	}
+                                          JLayer<? extends V> l) {
+    }
 
-	/**
-	 * Processes {@code KeyEvent} occurring on the {@link JLayer} or any of its
-	 * subcomponents.
-	 * 
-	 * @param e
-	 *            the {@code KeyEvent} to be processed
-	 * @param l
-	 *            the layer this LayerUI is set to
-	 */
-	@Override
+    /**
+     * Processes {@code KeyEvent} occurring on the {@link JLayer} or any of its
+     * subcomponents.
+     *
+     * @param e the {@code KeyEvent} to be processed
+     * @param l the layer this LayerUI is set to
+     */
+    @Override
     protected void processKeyEvent(KeyEvent e, JLayer<? extends V> l) {
-	}
+    }
 }
