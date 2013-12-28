dca062cf28f055e49650b2d443394bbb03e12d29
Craig Cavanaugh <jgnash.devel@gmail.com>
2013-12-28 07:08:33 -0500

    Dump unused code

--- jgnash-swing/src/main/java/jgnash/ui/wizards/file/NewFileDialog.java
+++ jgnash-swing/src/main/java/jgnash/ui/wizards/file/NewFileDialog.java
@@ -49,8 +49,6 @@ import jgnash.util.Resource;
 public class NewFileDialog extends WizardDialog {
 
     public enum Settings {
-
-        BACKEND,
         CURRENCIES,
         DEFAULT_CURRENCIES,
         DEFAULT_CURRENCY,
