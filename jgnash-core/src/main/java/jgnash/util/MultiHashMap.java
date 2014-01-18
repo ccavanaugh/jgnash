05cc6435eaa994c0ec88d88e284663393a8dc2a5
Craig Cavanaugh <jgnash.devel@gmail.com>
2014-01-18 07:10:12 -0500

    Code cleanup

--- jgnash-core/src/main/java/jgnash/util/MultiHashMap.java
+++ jgnash-core/src/main/java/jgnash/util/MultiHashMap.java
@@ -144,7 +144,7 @@ public class MultiHashMap<K, V> extends HashMap<K, Object> {
      * @return <code>true</code> if the map changed
      */
     @SuppressWarnings("rawtypes")
-    public Object remove(final K key, final V value) {
+    public boolean remove(final K key, final V value) {
 
         boolean result = false;
 
