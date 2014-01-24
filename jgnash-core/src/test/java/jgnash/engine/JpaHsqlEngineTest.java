7ecfb3c47b92dad6f3f02995a78ff33cf0562fb0
Craig Cavanaugh <jgnash.devel@gmail.com>
2014-01-24 05:14:24 -0500

    Cleanup

--- jgnash-core/src/test/java/jgnash/engine/JpaHsqlEngineTest.java
+++ jgnash-core/src/test/java/jgnash/engine/JpaHsqlEngineTest.java
@@ -24,7 +24,6 @@ import org.junit.Test;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.Assert.assertEquals;
