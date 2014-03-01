868d81653b48ff588a852d72d1071df87bf62bf8
Craig Cavanaugh <jgnash.devel@gmail.com>
2014-03-01 18:42:51 -0500

    Beginning framework for a JavaFX ui

---
+++ jgnash-fx/src/main/java/jgnash/uifx/MainApplication.java
@@ -0,0 +1,74 @@
+/*
+ * jGnash, a personal finance application
+ * Copyright (C) 2001-2014 Craig Cavanaugh
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jgnash.uifx;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import jgnash.MainFX;
+import jgnash.uifx.utils.StageUtils;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ * JavaFX version of jGnash.
+ *
+ * @author Craig Cavanaugh
+ */
+public class MainApplication extends Application
+{
+    //private final Resource rb = Resource.get();
+
+    private static final Logger logger = Logger.getLogger(MainApplication.class.getName());
+
+    @Override
+    public void start(final Stage stage) throws Exception
+    {
+
+        MenuBar menuBar = FXMLLoader.load(MainFX.class.getResource("fxml/mainMenu.fxml"));
+
+        VBox root = new VBox();
+        Scene scene = new Scene(root, 300, 150);
+
+        root.getChildren().add(menuBar);
+
+        stage.setTitle(MainFX.VERSION);
+        stage.setScene(scene);
+        stage.setResizable(true);
+
+        StageUtils.addBoundsListener(stage);
+
+        stage.show();
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Shutting down");
+
+        try {
+            super.stop();
+        } catch (final Exception e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+        }
+    }
+}
---
+++ jgnash-fx/src/main/java/jgnash/uifx/utils/StageUtils.java
@@ -0,0 +1,64 @@
+/*
+ * jGnash, a personal finance application
+ * Copyright (C) 2001-2014 Craig Cavanaugh
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jgnash.uifx.utils;
+
+import java.awt.geom.Rectangle2D;
+import java.util.prefs.Preferences;
+
+import jgnash.util.EncodeDecode;
+
+import javafx.stage.Stage;
+
+/**
+ * Saves and restores Stage sizes
+ *
+ * @author Craig Cavanaugh
+ */
+public class StageUtils {
+
+    public static void addBoundsListener(final Stage stage) {
+        addBoundsListener(stage, "bounds");
+    }
+
+    public static void addBoundsListener(final Stage stage, final String key) {
+        addBoundsListener(stage, stage.getClass().getName().replace('.', '/'), key);
+    }
+
+    private static void addBoundsListener(final Stage stage, final String prefNode, final String key) {
+        final String bounds = Preferences.userRoot().node(prefNode).get(key, null);
+
+        if (bounds != null) { // restore to previous size and position
+            final Rectangle2D.Double rectangle = EncodeDecode.decodeRectangle2D(bounds);
+
+            stage.setX(rectangle.getX());
+            stage.setY(rectangle.getY());
+
+            if (stage.isResizable()) {
+                stage.setWidth(rectangle.getWidth());
+                stage.setHeight(rectangle.getHeight());
+            }
+        }
+
+        stage.setOnCloseRequest(windowEvent -> {
+            final Preferences p = Preferences.userRoot().node(prefNode);
+            p.put(key, EncodeDecode.encodeRectangle2D(stage.getX(), stage.getY(), stage.getWidth(), stage.getHeight()));
+
+            //stage.onCloseRequestProperty().removeListener(this);  // make gc easier
+        });
+    }
+}
