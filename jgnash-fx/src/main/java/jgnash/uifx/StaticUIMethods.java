2c8d366b50f944708d616c7605f8e2df06b80181
Craig Cavanaugh <jgnash.devel@gmail.com>
2014-03-11 19:17:01 -0400

    Use css to standardize layout spacings

--- jgnash-fx/src/main/java/jgnash/uifx/StaticUIMethods.java
+++ jgnash-fx/src/main/java/jgnash/uifx/StaticUIMethods.java
@@ -59,6 +59,9 @@ public class StaticUIMethods {
 
             dialog.setResizable(false);
 
+            dialog.getScene().getStylesheets().add(MainApplication.DEFAULT_CSS);
+            dialog.getScene().getRoot().getStyleClass().addAll("form", "dialog");
+
             StageUtils.addBoundsListener(dialog, StaticUIMethods.class);
 
             dialog.show();
