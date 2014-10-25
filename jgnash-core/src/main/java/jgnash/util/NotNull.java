80f66e680e553ec4a5e20cf40c3d3741b6942397
Craig Cavanaugh <jgnash.devel@gmail.com>
2014-10-25 15:59:36 -0400

    Add Nullable and NotNull annotations

---
+++ jgnash-core/src/main/java/jgnash/util/NotNull.java
@@ -0,0 +1,14 @@
+package jgnash.util;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE})
+public @interface NotNull {
+
+}
