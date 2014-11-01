ffc434c865b20b2762d8aa8130d25a6deadf52cc
Craig Cavanaugh <jgnash.devel@gmail.com>
2014-11-01 04:49:18 -0400

    Add stress test for EncryptionManager

---
+++ jgnash-core/src/test/java/jgnash/util/EncryptionManagerTest.java
@@ -0,0 +1,48 @@
+/*
+ * jGnash, a personal finance application
+ * Copyright (C) 2001-2014 Craig Cavanaugh
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jgnash.util;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * EncryptionManager test
+ *
+ * @author Craig Cavanaugh
+ */
+public class EncryptionManagerTest {
+
+    public static final String PASSWORD = RandomStringUtils.random(20);
+
+    @Test
+    public void test() {
+        final EncryptionManager encryptionManager = new EncryptionManager(PASSWORD.toCharArray());
+
+        for (int i = 1; i < 8192; i++) {
+            String testString = RandomStringUtils.random(i);
+
+            final String encrypted = encryptionManager.encrypt(testString);
+
+            final String decrypted = encryptionManager.decrypt(encrypted);
+
+            assertEquals(testString, decrypted);
+        }
+    }
+}
