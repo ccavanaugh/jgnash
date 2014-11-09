edb5ce1a76327d644f96cd42e8435690a5f51228
Craig Cavanaugh <jgnash.devel@gmail.com>
2014-11-09 06:33:11 -0500

    Add new control for better handling of long running tasks

---
+++ jgnash-fx/src/main/java/jgnash/uifx/control/BusyPane.java
@@ -0,0 +1,95 @@
+/*
+ * jGnash, a personal finance application
+ * Copyright (C) 2001-2014 Craig Cavanaugh
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jgnash.uifx.control;
+
+import jgnash.util.Nullable;
+
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.effect.BoxBlur;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+
+/**
+ * A busy pane.  Intended to overlay the main ui and blur the display when a long running operation is occurring
+ *
+ * @author Craig Cavanaugh
+ */
+public class BusyPane extends StackPane {
+
+    private static final String BUSY_PANE_STYLE = "busy-pane";
+
+    private ImageView imageView;
+
+    private Label messageLabel;
+
+    private ProgressIndicator progressIndicator;
+
+    public BusyPane() {
+
+        getStyleClass().add(BUSY_PANE_STYLE);
+
+        imageView = new ImageView();
+        imageView.setFocusTraversable(false);
+        imageView.setEffect(new BoxBlur(4, 4, 2));
+
+        progressIndicator = new ProgressIndicator();
+
+        messageLabel = new Label();
+        messageLabel.setGraphic(progressIndicator);
+
+        getChildren().addAll(messageLabel);
+
+        setVisible(false);
+    }
+
+    public void setTask(@Nullable final Task<?> task) {
+        if (task == null) {
+            setVisible(false);
+            progressIndicator.progressProperty().unbind();
+            messageLabel.textProperty().unbind();
+        } else {
+            messageLabel.textProperty().bind(task.messageProperty());
+            progressIndicator.progressProperty().bind(task.progressProperty());
+
+            // Add event handlers to automatically hide the busy pane.
+            // These handler will not override the setOnXxx handlers
+            task.addEventHandler(WorkerStateEvent.WORKER_STATE_SUCCEEDED, event -> setTask(null));
+            task.addEventHandler(WorkerStateEvent.WORKER_STATE_CANCELLED, event -> setTask(null));
+            task.addEventHandler(WorkerStateEvent.WORKER_STATE_FAILED, event -> setTask(null));
+
+            setVisible(true);
+        }
+    }
+
+    @Override protected void layoutChildren() {
+        super.layoutChildren();
+        if (getParent() != null && isVisible()) {
+            setVisible(false);
+            getChildren().remove(imageView);
+
+            imageView.setImage(getScene().getRoot().snapshot(null, null));
+
+            getChildren().add(imageView);
+            imageView.toBack();
+            setVisible(true);
+        }
+    }
+}
