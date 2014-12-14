e3953cf3dcb27d91e8ec4ba8d2eeafe144c9a71e
Craig Cavanaugh <jgnash.devel@gmail.com>
2014-12-14 18:55:11 -0500

    Rough implementation of the transaction form

--- jgnash-fx/src/main/java/jgnash/uifx/views/register/RegisterPaneController.java
+++ jgnash-fx/src/main/java/jgnash/uifx/views/register/RegisterPaneController.java
@@ -24,14 +24,20 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import jgnash.engine.Account;
+import jgnash.uifx.MainApplication;
 
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 
 /**
@@ -44,13 +50,13 @@ public class RegisterPaneController implements Initializable {
     //private ResourceBundle resources;
 
     @FXML
-    public StackPane register;
+    protected StackPane register;
 
     @FXML
-    public ButtonBar buttonBar;
+    protected ButtonBar buttonBar;
 
     @FXML
-    public StackPane forms;
+    protected TabPane transactionForms;
 
     /**
      * Active account for the pane
@@ -75,6 +81,9 @@ public class RegisterPaneController implements Initializable {
 
         buttonBar.getButtons().addAll(newButton, duplicateButton, jumpButton, deleteButton);
 
+        transactionForms.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+
+        // Load the register table
         try {
             final FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("RegisterTable.fxml"), resources);
             register.getChildren().add(fxmlLoader.load());
@@ -82,6 +91,56 @@ public class RegisterPaneController implements Initializable {
 
             // Bind  the register pane to this account property
             registerTableController.getAccountProperty().bind(accountProperty);
+        } catch (final IOException e) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getLocalizedMessage(), e);
+        }
+
+        try {
+            final FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("TransactionPane.fxml"), resources);
+
+            final Pane pane = fxmlLoader.load();
+
+            TransactionPanelController transactionPanelController = fxmlLoader.getController();
+
+            accountProperty.addListener(new ChangeListener<Account>() {
+                @Override
+                public void changed(ObservableValue<? extends Account> observable, Account oldValue, Account newValue) {
+                    transactionPanelController.getAccountProperty().setValue(newValue);
+                }
+            });
+
+            pane.getStylesheets().add(MainApplication.DEFAULT_CSS);
+            pane.getStyleClass().addAll("form", "dialog");
+
+            final Tab tab = new Tab("Increase");
+            tab.setContent(pane);
+
+            transactionForms.getTabs().addAll(tab);
+        } catch (final IOException e) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getLocalizedMessage(), e);
+        }
+
+        try {
+            final FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("TransactionPane.fxml"), resources);
+
+            final Pane pane = fxmlLoader.load();
+
+            TransactionPanelController transactionPanelController = fxmlLoader.getController();
+
+            accountProperty.addListener(new ChangeListener<Account>() {
+                @Override
+                public void changed(ObservableValue<? extends Account> observable, Account oldValue, Account newValue) {
+                    transactionPanelController.getAccountProperty().setValue(newValue);
+                }
+            });
+
+            pane.getStylesheets().add(MainApplication.DEFAULT_CSS);
+            pane.getStyleClass().addAll("form", "dialog");
+
+            final Tab tab = new Tab("Decrease");
+            tab.setContent(pane);
+
+            transactionForms.getTabs().addAll(tab);
         } catch (final IOException e) {
             Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getLocalizedMessage(), e);
         }
---
+++ jgnash-fx/src/main/java/jgnash/uifx/views/register/TransactionPanelController.java
@@ -0,0 +1,132 @@
+/*
+ * jGnash, a personal finance application
+ * Copyright (C) 2001-2014 Craig Cavanaugh
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jgnash.uifx.views.register;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+
+import jgnash.engine.Account;
+import jgnash.uifx.MainApplication;
+import jgnash.uifx.control.AccountComboBox;
+import jgnash.uifx.control.DatePickerEx;
+import jgnash.uifx.control.DecimalTextField;
+import jgnash.uifx.control.TransactionNumberComboBox;
+
+import org.controlsfx.glyphfont.FontAwesome;
+import org.controlsfx.glyphfont.GlyphFont;
+import org.controlsfx.glyphfont.GlyphFontRegistry;
+
+/**
+ * Transaction Entry Controller
+ *
+ * @author Craig Cavanaugh
+ */
+public class TransactionPanelController implements Initializable {
+
+    @FXML
+    private GridPane commandPane;
+
+    @FXML
+    protected TextField payeeTextField;
+
+    @FXML
+    protected AccountComboBox accountComboBox;
+
+    @FXML
+    protected Button splitsButton;
+
+    @FXML
+    protected TransactionNumberComboBox numberComboBox;
+
+    @FXML
+    protected DatePickerEx datePicker;
+
+    @FXML
+    protected DecimalTextField amountTextField;
+
+    @FXML
+    protected TextField memoTextField;
+
+    @FXML
+    protected HBox exchangePanel;
+
+    @FXML
+    protected ButtonBar buttonBar;
+
+    @FXML
+    protected CheckBox reconciledButton;
+
+    @FXML
+    protected Button attachmentButton;
+
+    @FXML
+    protected Button viewAttachmentButton;
+
+    @FXML
+    protected Button deleteAttachmentButton;
+
+    final private ObjectProperty<Account> accountProperty = new SimpleObjectProperty<>();
+
+    @Override
+    public void initialize(final URL location, final ResourceBundle resources) {
+
+        buttonBar.getStylesheets().add(MainApplication.DEFAULT_CSS);
+
+        // Create and add the ok and cancel buttons to the button bar
+        final Button okButton = new Button(resources.getString("Button.Ok"));
+        final Button cancelButton = new Button(resources.getString("Button.Cancel"));
+
+        ButtonBar.setButtonData(okButton, ButtonBar.ButtonData.OK_DONE);
+        ButtonBar.setButtonData(cancelButton, ButtonBar.ButtonData.CANCEL_CLOSE);
+        buttonBar.getButtons().addAll(okButton, cancelButton);
+
+        final GlyphFont fontAwesome = GlyphFontRegistry.font("FontAwesome");
+
+        attachmentButton.setGraphic(fontAwesome.create(FontAwesome.Glyph.ANCHOR));
+        viewAttachmentButton.setGraphic(fontAwesome.create(FontAwesome.Glyph.EYE));
+        deleteAttachmentButton.setGraphic(fontAwesome.create(FontAwesome.Glyph.TRASH_ALT));
+
+        commandPane.getStylesheets().add(MainApplication.DEFAULT_CSS);
+        commandPane.getStyleClass().addAll("form");
+
+        getAccountProperty().addListener(new ChangeListener<Account>() {
+            @Override
+            public void changed(final ObservableValue<? extends Account> observable, final Account oldValue, final Account newValue) {
+                numberComboBox.setAccount(newValue);
+            }
+        });
+
+    }
+
+    ObjectProperty<Account> getAccountProperty() {
+        return accountProperty;
+    }
+}
