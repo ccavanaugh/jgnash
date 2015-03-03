b65784d529e127d644781660059478a7ad83b18f
Craig Cavanaugh <jgnash.devel@gmail.com>
2015-03-03 05:41:02 -0500

    Fix focus when cleared

--- jgnash-fx/src/main/java/jgnash/uifx/views/register/AbstractPriceQtyInvSlipController.java
+++ jgnash-fx/src/main/java/jgnash/uifx/views/register/AbstractPriceQtyInvSlipController.java
@@ -83,7 +83,7 @@ public abstract class AbstractPriceQtyInvSlipController extends AbstractInvSlipC
 
     @Override
     protected void focusFirstComponent() {
-        memoTextField.requestFocus();
+        priceField.requestFocus();
     }
 
     @Override
