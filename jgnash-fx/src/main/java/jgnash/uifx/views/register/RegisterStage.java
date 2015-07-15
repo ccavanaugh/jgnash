fd21e69b564155b50bcd9ef2733aa9a6b8a3f2ac
Craig Cavanaugh <jgnash.devel@gmail.com>
2015-07-15 05:49:09 -0400

    WIP for register stages/windows

---
+++ jgnash-fx/src/main/java/jgnash/uifx/views/register/RegisterStage.java
@@ -0,0 +1,10 @@
+package jgnash.uifx.views.register;
+
+/**
+ * A Stage that displays a single account register. Size and position is preserved
+ *
+ * @author Craig Cavanaugh
+ */
+public class RegisterStage {
+
+}
