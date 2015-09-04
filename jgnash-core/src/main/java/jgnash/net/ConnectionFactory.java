f71dacc88bcf45b88725e14d56fbac04b0be82d5
Craig Cavanaugh <jgnash.devel@gmail.com>
2015-09-04 05:45:42 -0400

    constants should be public for setting UI parameters

--- jgnash-core/src/main/java/jgnash/net/ConnectionFactory.java
+++ jgnash-core/src/main/java/jgnash/net/ConnectionFactory.java
@@ -38,9 +38,9 @@ public class ConnectionFactory {
 
     private static final int DEFAULT_TIMEOUT = 30;
 
-    private static final int MAX_TIMEOUT = 30;
+    public static final int MAX_TIMEOUT = 30;
 
-    private static final int MIN_TIMEOUT = 1;
+    public static final int MIN_TIMEOUT = 1;
 
     private ConnectionFactory() {
     }
