bea50d6eed0384a91e4b42c505b29157a340a737
Craig Cavanaugh <jgnash.devel@gmail.com>
2015-12-12 07:44:11 -0500

    Move the report generation into the core for reuse

---
+++ jgnash-core/src/main/java/jgnash/report/ProfitLossTextReport.java
@@ -0,0 +1,241 @@
+/*
+ * jGnash, a personal finance application
+ * Copyright (C) 2001-2015 Craig Cavanaugh
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jgnash.report;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.ResourceBundle;
+import java.util.function.BiFunction;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import jgnash.engine.Account;
+import jgnash.engine.AccountType;
+import jgnash.engine.Comparators;
+import jgnash.engine.CurrencyNode;
+import jgnash.engine.Engine;
+import jgnash.engine.EngineFactory;
+import jgnash.text.CommodityFormat;
+import jgnash.util.ResourceUtils;
+
+/**
+ * Dumps a simple Profit/Loss report to a text file
+ *
+ * @author Michael Mueller
+ * @author Craig Cavanaugh
+ * @author David Robertson
+ */
+public class ProfitLossTextReport {
+
+    private static final int MAX_LENGTH = 15;  //(-000,000,000.00)
+
+    private static final int MAX_NAME_LEN = 30;
+
+    private static final String REPORT_FOOTER = "======================================================";
+
+    private static final String ROW_SEPARATOR = "------------------------------------------------------";
+
+    private NumberFormat numberFormat;
+
+    private final List<String> reportText = new ArrayList<>();
+
+    private final CurrencyNode baseCommodity;
+
+    private final ResourceBundle rb = ResourceUtils.getBundle();
+
+    private final BiFunction<AccountType, BigDecimal, BigDecimal> balanceConverter;
+
+    private final LocalDate startDate;
+
+    private final LocalDate endDate;
+
+    private final String fileName;
+
+    /**
+     * Report constructor
+     *
+     * @param fileName         file name to save to
+     * @param startDate        start date for the report
+     * @param endDate          end date for the report
+     * @param currencyNode     CurrencyNode account balances are reported in
+     * @param balanceConverter function to adjust account balance sign
+     */
+    public ProfitLossTextReport(final String fileName, final LocalDate startDate, final LocalDate endDate,
+                                final CurrencyNode currencyNode,
+                                final BiFunction<AccountType, BigDecimal, BigDecimal> balanceConverter) {
+
+        Objects.requireNonNull(fileName);
+
+        this.fileName = fileName;
+        this.baseCommodity = currencyNode;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.balanceConverter = balanceConverter;
+    }
+
+    public void run() {
+        generateReport();
+        writeFile();
+    }
+
+    private void generateReport() {
+        final Engine engine = EngineFactory.getEngine(EngineFactory.DEFAULT);
+        Objects.requireNonNull(engine);
+
+        final Account root = engine.getRootAccount();
+
+        numberFormat = CommodityFormat.getFullNumberFormat(baseCommodity);
+
+        final DateTimeFormatter df = DateTimeFormatter.ISO_DATE;
+
+        // title and dates
+        reportText.add(rb.getString("Title.ProfitLoss"));
+        reportText.add("");
+        reportText.add("From " + df.format(startDate) + " To " + df.format(endDate));
+        reportText.add("");
+        reportText.add("");
+
+        //Income
+        reportText.add(AccountType.INCOME.toString());
+        reportText.add(ROW_SEPARATOR);
+
+        //Add up the  Gross Income.
+        BigDecimal incomeTotal = BigDecimal.ZERO;
+
+        for (final BigDecimal balance : getBalances(root, AccountType.INCOME)) {
+            incomeTotal = incomeTotal.add(balance);
+        }
+
+        reportText.add(ROW_SEPARATOR);
+        reportText.add(formatAccountName(rb.getString("Word.GrossIncome")) + " " + formatDecimal(incomeTotal));
+        reportText.add(ROW_SEPARATOR);
+        reportText.add("");
+        reportText.add("");
+
+        //Expense
+        reportText.add(AccountType.EXPENSE.toString());
+        reportText.add(ROW_SEPARATOR);
+
+        //Add up the Gross Expenses
+        BigDecimal expenseTotal = BigDecimal.ZERO;
+
+        for (final BigDecimal balance : getBalances(root, AccountType.EXPENSE)) {
+            expenseTotal = expenseTotal.add(balance);
+        }
+        reportText.add(ROW_SEPARATOR);
+        reportText.add(formatAccountName(rb.getString("Word.GrossExpense")) + " " + formatDecimal(expenseTotal));
+        reportText.add(ROW_SEPARATOR);
+        reportText.add("");
+        reportText.add("");
+
+        //Net Total
+        reportText.add(ROW_SEPARATOR);
+        reportText.add(formatAccountName(rb.getString("Word.NetIncome")) + " " + formatDecimal(incomeTotal.add(expenseTotal)));
+        reportText.add(REPORT_FOOTER);
+    }
+
+    private void writeFile() {
+        try (final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName),
+                StandardCharsets.UTF_8))) {
+            for (final String text : reportText) {  //write the array list pl to the file
+                writer.write(text);
+                writer.newLine();
+            }
+            writer.newLine();
+        } catch (IOException e) {
+            Logger.getLogger(ProfitLossTextReport.class.getName()).log(Level.SEVERE, e.getLocalizedMessage(), e);
+        }
+    }
+
+    private List<BigDecimal> getBalances(final Account a, final AccountType type) {
+
+        final List<BigDecimal> balances = new ArrayList<>();
+
+        for (final Account child : a.getChildren(Comparators.getAccountByCode())) {
+            if ((child.getTransactionCount() > 0) && type == child.getAccountType()) {
+
+                final BigDecimal acctBal = balanceConverter.apply(child.getAccountType(),
+                        child.getBalance(startDate, endDate, baseCommodity));
+
+                // output account name and balance
+                reportText.add(formatAccountName(child.getName()) + " " + formatDecimal(acctBal));
+
+                balances.add(acctBal);
+            }
+
+            if (child.isParent()) {
+                balances.addAll(getBalances(child, type));
+            }
+        }
+
+        return balances;
+    }
+
+    /**
+     * Format decimal amount
+     *
+     * @param balance the BigDecimal value to format
+     * @return formatted string
+     */
+    private String formatDecimal(final BigDecimal balance) {
+        final StringBuilder stringBuilder = new StringBuilder();
+        final String formattedBalance = numberFormat.format(balance);
+
+        // right align amount to pre-defined maximum length
+        if (formattedBalance.length() < MAX_LENGTH) {
+            for (int i = formattedBalance.length(); i < MAX_LENGTH; i++) {
+                stringBuilder.append(' ');
+            }
+        }
+
+        stringBuilder.append(formattedBalance);
+
+        return stringBuilder.toString();
+    }
+
+    /**
+     * Format account name
+     *
+     * @param name the account name to format
+     * @return the formatted account name
+     */
+    private static String formatAccountName(final String name) {
+        final StringBuilder stringBuilder = new StringBuilder(MAX_NAME_LEN);
+
+        stringBuilder.append(name);
+
+        // set name to pre-defined maximum length
+        for (int i = name.length(); i < MAX_NAME_LEN; i++) {
+            stringBuilder.append(' ');
+        }
+
+        stringBuilder.setLength(MAX_NAME_LEN);
+
+        return stringBuilder.toString();
+    }
+}
--- jgnash-swing/src/main/java/jgnash/ui/report/compiled/ProfitLossTXT.java
+++ jgnash-swing/src/main/java/jgnash/ui/report/compiled/ProfitLossTXT.java
@@ -17,33 +17,19 @@
  */
 package jgnash.ui.report.compiled;
 
-import java.io.BufferedWriter;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.math.BigDecimal;
-import java.nio.charset.StandardCharsets;
-import java.text.NumberFormat;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.Objects;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-import jgnash.engine.Account;
-import jgnash.engine.AccountType;
-import jgnash.engine.Comparators;
 import jgnash.engine.CurrencyNode;
 import jgnash.engine.Engine;
 import jgnash.engine.EngineFactory;
-import jgnash.text.CommodityFormat;
+import jgnash.report.ProfitLossTextReport;
 import jgnash.ui.components.JDateField;
 import jgnash.ui.register.AccountBalanceDisplayManager;
 import jgnash.util.ResourceUtils;
@@ -60,116 +46,37 @@ import com.jgoodies.forms.layout.FormLayout;
  */
 public class ProfitLossTXT {
 
-    private NumberFormat numberFormat;
-
-    private ArrayList<BigDecimal> balance = new ArrayList<>();
-
-    private final ArrayList<String> pl = new ArrayList<>();
-
-    private CurrencyNode baseCommodity;
-
-    private LocalDate[] dates;
-
     private final ResourceBundle rb = ResourceUtils.getBundle();
 
     public void run() {
-        createReport();
-        writePLFile(getFileName());
-    }
-
-    private void createReport() {
         final Engine engine = EngineFactory.getEngine(EngineFactory.DEFAULT);
         Objects.requireNonNull(engine);
 
-        final Account root = engine.getRootAccount();
+        final CurrencyNode baseCommodity = engine.getDefaultCurrency();
 
-        baseCommodity = engine.getDefaultCurrency();
+        final LocalDate[] dates = getDates();
 
-        numberFormat = CommodityFormat.getFullNumberFormat(baseCommodity);
+        if (dates != null) {
+            final String fileName = getFileName();
 
-        DateTimeFormatter df = DateTimeFormatter.ISO_DATE;
+            ProfitLossTextReport report = new ProfitLossTextReport(fileName, dates[0], dates[1], baseCommodity,
+                    AccountBalanceDisplayManager::convertToSelectedBalanceMode);
 
-        dates = getDates();
-
-        // title and dates
-        pl.add(rb.getString("Title.ProfitLoss"));
-        pl.add("");
-        pl.add("From " + df.format(dates[0]) + " To " + df.format(dates[1]));
-        pl.add("");
-        pl.add("");
-
-        //Income
-        pl.add("Income");
-        pl.add("------------------------------------------------------");
-        getBalances(root, dates, AccountType.INCOME);
-
-        //Add up the  Gross Income.
-        BigDecimal total1 = BigDecimal.ZERO;
-        for (Object aBalance1 : balance) {
-            total1 = total1.add((BigDecimal) aBalance1);
+            report.run();
         }
-        pl.add("------------------------------------------------------");
-        pl.add(formatAcctNameOut(rb.getString("Word.GrossIncome")) + " " + formatDecimalOut(total1));
-        pl.add("------------------------------------------------------");
-        pl.add("");
-        pl.add("");
-
-        //Expense
-        pl.add("Expenses");
-        pl.add("------------------------------------------------------");
-        balance = new ArrayList<>();
-        getBalances(root, dates, AccountType.EXPENSE);
-
-        //Add up the Gross Expenses
-        BigDecimal total2 = BigDecimal.ZERO;
-        for (Object aBalance : balance) {
-            total2 = total2.add((BigDecimal) aBalance);
-        }
-        pl.add("------------------------------------------------------");
-        pl.add(formatAcctNameOut(rb.getString("Word.GrossExpense")) + " " + formatDecimalOut(total2));
-        pl.add("------------------------------------------------------");
-        pl.add("");
-        pl.add("");
-
-        //Net Total
-        pl.add("------------------------------------------------------");
-        pl.add(formatAcctNameOut(rb.getString("Word.NetIncome")) + " " + formatDecimalOut(total1.add(total2)));
-        pl.add("======================================================");
-
-    }
-
-    private void writePLFile(final String fileName) {
-        if (fileName == null || dates == null) {
-            return;
-        }
-
-        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName), StandardCharsets.UTF_8))) {
-            for (Object aPl : pl) {  //write the array list pl to the file
-                //print (pl.get(i));
-                writer.write(aPl.toString());
-                writer.newLine();
-            }
-            writer.newLine();            
-        } catch (IOException e) {
-            Logger.getLogger(ProfitLossTXT.class.getName()).log(Level.SEVERE, e.getLocalizedMessage(), e);
-        }         
-    }
-
-    private static LocalDate[] getLastDays(final LocalDate start, final LocalDate stop) {
-        return new LocalDate[] {start, stop};
     }
 
     private LocalDate[] getDates() {
 
-        LocalDate start = LocalDate.now().minusYears(1);
+        final LocalDate start = LocalDate.now().minusYears(1);
 
-        JDateField startField = new JDateField();
-        JDateField endField = new JDateField();
+        final JDateField startField = new JDateField();
+        final JDateField endField = new JDateField();
 
         startField.setValue(start);
 
-        FormLayout layout = new FormLayout("right:p, 4dlu, p:g", "");
-        DefaultFormBuilder builder = new DefaultFormBuilder(layout);
+        final FormLayout layout = new FormLayout("right:p, 4dlu, p:g", "");
+        final DefaultFormBuilder builder = new DefaultFormBuilder(layout);
         builder.rowGroupingEnabled(true);
 
         builder.append(rb.getString("Label.StartDate"), startField);
@@ -178,19 +85,20 @@ public class ProfitLossTXT {
         builder.appendUnrelatedComponentsGapRow();
         builder.nextLine();
 
-        JPanel panel = builder.getPanel();
+        final JPanel panel = builder.getPanel();
 
-        int option = JOptionPane.showConfirmDialog(null, new Object[] { panel }, rb.getString("Message.StartEndDate"), JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+        int option = JOptionPane.showConfirmDialog(null, new Object[] { panel }, rb.getString("Message.StartEndDate"),
+                JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
 
         if (option == JOptionPane.OK_OPTION) {
-            return getLastDays(startField.localDateValue(), endField.localDateValue());
+            return new LocalDate[] {startField.localDateValue(), endField.localDateValue()};
         }
 
         return null;
     }
 
     private String getFileName() {
-        JFileChooser chooser = new JFileChooser();
+        final JFileChooser chooser = new JFileChooser();
         chooser.setMultiSelectionEnabled(false);
         chooser.addChoosableFileFilter(new FileNameExtensionFilter(rb.getString("Message.TXTFile"), "txt"));
 
@@ -202,73 +110,5 @@ public class ProfitLossTXT {
             return fileName;
         }
         return null;
-    }
-
-    /**
-     * format output decimal amount
-     * 
-     * @param amt the BigDecimal value to format
-     * @return formatted string
-     */
-    private String formatDecimalOut(final BigDecimal amt) {
-
-        int maxLen = 15; //(-000,000,000.00)
-        StringBuilder sb = new StringBuilder();
-
-        String formattedAmt = numberFormat.format(amt);
-
-        // right align amount to pre-defined maximum length (maxLen)
-        int amtLen = formattedAmt.length();
-        if (amtLen < maxLen) {
-            for (int ix = amtLen; ix < maxLen; ix++) {
-                sb.append(' ');
-            }
-        }
-
-        sb.append(formattedAmt);
-
-        return sb.toString();
-    }
-
-    /**
-     * format output account name
-     * 
-     * @param acctName the account name to format
-     * @return the formatted account name
-     */
-    private static String formatAcctNameOut(final String acctName) {
-
-        int maxLen = 30; // max 30 characters
-        StringBuilder sb = new StringBuilder(maxLen);
-
-        sb.append(acctName);
-
-        // set name to pre-defined maximum length (maxLen)
-        int nameLen = acctName.length();
-        for (int ix = nameLen; ix < maxLen; ix++) {
-            sb.append(' ');
-        }
-        sb.setLength(maxLen);
-
-        return sb.toString();
-    }
-
-    private void getBalances(final Account a, final LocalDate[] dates1, final AccountType type) {
-
-        for (final Account child : a.getChildren(Comparators.getAccountByCode())) {
-            if ((child.getTransactionCount() > 0) && type == child.getAccountType()) {
-                String acctName = child.getName();
-
-                BigDecimal acctBal = AccountBalanceDisplayManager.convertToSelectedBalanceMode(child.getAccountType(), child.getBalance(dates1[0], dates1[1], baseCommodity));
-
-                // output account name and balance
-                pl.add(formatAcctNameOut(acctName) + " " + formatDecimalOut(acctBal));
-
-                balance.add(acctBal);
-            }
-            if (child.isParent()) {
-                getBalances(child, dates1, type);
-            }
-        }
     }
 }
