244c0f24f009d4e9f746d4ccf0784fded844b8d5
Craig Cavanaugh <jgnash.devel@gmail.com>
2016-05-30 06:55:46 -0400

    WIP for income and expense pie charts by payee

--- jgnash-fx/src/main/java/jgnash/uifx/report/IncomeExpensePayeePieChartDialogController.java
+++ jgnash-fx/src/main/java/jgnash/uifx/report/IncomeExpensePayeePieChartDialogController.java
@@ -17,7 +17,13 @@
  */
 package jgnash.uifx.report;
 
+import java.math.BigDecimal;
 import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.ResourceBundle;
 import java.util.prefs.Preferences;
@@ -30,24 +36,21 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.geometry.Side;
-import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
-import javafx.scene.control.Tooltip;
 import javafx.stage.Stage;
 
 import jgnash.engine.Account;
-import jgnash.engine.AccountType;
+import jgnash.engine.Comparators;
 import jgnash.engine.CurrencyNode;
 import jgnash.engine.Engine;
 import jgnash.engine.EngineFactory;
-import jgnash.resource.cursor.CustomCursor;
-import jgnash.text.CommodityFormat;
+import jgnash.engine.Transaction;
 import jgnash.uifx.Options;
 import jgnash.uifx.control.AccountComboBox;
 import jgnash.uifx.control.DatePickerEx;
 import jgnash.uifx.util.InjectFXML;
-import jgnash.util.function.ParentAccountPredicate;
+import jgnash.util.NotNull;
 
 /**
  * Income and Expense Payee Pie Chart.
@@ -58,11 +61,24 @@ import jgnash.util.function.ParentAccountPredicate;
 @SuppressWarnings("WeakerAccess")
 public class IncomeExpensePayeePieChartDialogController {
 
+    private static final int CREDIT = 0;
+
+    private static final int DEBIT = 1;
+
+    private static final int MAX_NAME_LENGTH = 12;
+
+    private static final String ELLIPSIS = "…";
+
+    private static final String CHART_CSS = "jgnash/skin/incomeExpensePieChart.css";
+
     @InjectFXML
     private final ObjectProperty<Scene> parentProperty = new SimpleObjectProperty<>();
 
     @FXML
-    private PieChart pieChart;
+    private PieChart debitPieChart;
+
+    @FXML
+    private PieChart creditPieChart;
 
     @FXML
     private DatePickerEx startDatePicker;
@@ -76,27 +92,21 @@ public class IncomeExpensePayeePieChartDialogController {
     @FXML
     private ResourceBundle resources;
 
-    private boolean nodeFocused = false;
-
     private static final String LAST_ACCOUNT = "lastAccount";
 
     @FXML
     public void initialize() {
 
         // Respect animation preference
-        pieChart.animatedProperty().setValue(Options.animationsEnabledProperty().get());
+        debitPieChart.animatedProperty().setValue(Options.animationsEnabledProperty().get());
+        creditPieChart.animatedProperty().setValue(Options.animationsEnabledProperty().get());
 
-        accountComboBox.valueProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue != null && newValue.getParent().getAccountType() != AccountType.ROOT) {
-                pieChart.setCursor(CustomCursor.getZoomOutCursor());
-            } else {
-                pieChart.setCursor(Cursor.DEFAULT);
-            }
-        });
+        creditPieChart.getStylesheets().addAll(CHART_CSS);
+        debitPieChart.getStylesheets().addAll(CHART_CSS);
+
+        accountComboBox.setPredicate(AccountComboBox.getShowAllPredicate());
 
         final Preferences preferences = Preferences.userNodeForPackage(IncomeExpensePayeePieChartDialogController.class);
-
-        accountComboBox.setPredicate(new ParentAccountPredicate());
 
         if (preferences.get(LAST_ACCOUNT, null) != null) {
             final Engine engine = EngineFactory.getEngine(EngineFactory.DEFAULT);
@@ -122,14 +132,8 @@ public class IncomeExpensePayeePieChartDialogController {
         startDatePicker.valueProperty().addListener(listener);
         endDatePicker.valueProperty().addListener(listener);
 
-        pieChart.setLegendSide(Side.RIGHT);
-
-        // zoom out
-        pieChart.setOnMouseClicked(event -> {
-            if (!nodeFocused && accountComboBox.getValue().getParent().getAccountType() != AccountType.ROOT) {
-                accountComboBox.setValue(accountComboBox.getValue().getParent());
-            }
-        });
+        creditPieChart.setLegendSide(Side.BOTTOM);
+        debitPieChart.setLegendSide(Side.BOTTOM);
 
         // Push the initial load to the end of the platform thread for better startup and nicer visual effect
         Platform.runLater(this::updateChart);
@@ -142,67 +146,24 @@ public class IncomeExpensePayeePieChartDialogController {
         final Account a = accountComboBox.getValue();
 
         if (a != null) {
-            final CurrencyNode defaultCurrency = a.getCurrencyNode();
 
-            final NumberFormat numberFormat = CommodityFormat.getFullNumberFormat(defaultCurrency);
+            ObservableList<PieChart.Data>[] chartData = createPieDataSet(a);
 
-            final ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
-
-            double total = a.getTreeBalance(startDatePicker.getValue(), endDatePicker.getValue(),
-                    defaultCurrency).doubleValue();
-
-            for (final Account child : a.getChildren()) {
-                double balance = child.getTreeBalance(startDatePicker.getValue(), endDatePicker.getValue(),
-                        defaultCurrency).doubleValue();
-
-                if (balance > 0 || balance < 0) {
-                    final String label = child.getName() + " - " + numberFormat.format(balance);
-                    final PieChart.Data data = new PieChart.Data(label, balance / total * 100);
-
-                    // nodes are created lazily.  Set the user data (Account) after the node is created
-                    data.nodeProperty().addListener((observable, oldValue, newValue) -> newValue.setUserData(child));
-
-                    pieChartData.add(data);
-                }
-            }
-
-            pieChart.setData(pieChartData);
+            creditPieChart.setData(chartData[CREDIT]);
+            debitPieChart.setData(chartData[DEBIT]);
 
             final NumberFormat percentFormat = NumberFormat.getPercentInstance();
             percentFormat.setMaximumFractionDigits(1);
             percentFormat.setMinimumFractionDigits(1);
 
+            /*
+
             // Install tooltips on the data after it has been added to the chart
-            pieChart.getData().stream().forEach(data ->
+            creditPieChart.getData().stream().forEach(data ->
                     Tooltip.install(data.getNode(), new Tooltip((((Account) data.getNode().getUserData()).getName()
-                            + " - " + percentFormat.format(data.getPieValue() / 100d)))));
+                            + " - " + percentFormat.format(data.getPieValue() / 100d))))); */
 
-            // Indicate the node can be clicked on to zoom into the next account level
-            for (final PieChart.Data data : pieChart.getData()) {
-                data.getNode().setOnMouseEntered(event -> {
-                    final Account account = (Account) data.getNode().getUserData();
-                    if (account.isParent()) {
-                        data.getNode().setCursor(CustomCursor.getZoomInCursor());
-                    } else {
-                        data.getNode().setCursor(Cursor.DEFAULT);
-                    }
-
-                    nodeFocused = true;
-                });
-
-                data.getNode().setOnMouseExited(event -> nodeFocused = false);
-
-                // zoom in on click if this is a parent account
-                data.getNode().setOnMouseClicked(event -> {
-                    if (data.getNode().getUserData() != null) {
-                        if (((Account) data.getNode().getUserData()).isParent()) {
-                            accountComboBox.setValue((Account) data.getNode().getUserData());
-                        }
-                    }
-                });
-            }
-
-            final String title;
+            /*final String title;
 
             // pick an appropriate title
             if (a.getAccountType() == AccountType.EXPENSE) {
@@ -213,30 +174,136 @@ public class IncomeExpensePayeePieChartDialogController {
                 title = resources.getString("Title.PercentDist");
             }
 
-            pieChart.setTitle(title + " - " + accountComboBox.getValue().getName() + " - " + numberFormat.format(total));
+            creditPieChart.setTitle(title + " - " + accountComboBox.getValue().getName() + " - " + numberFormat.format(total));*/
 
             // abs() on all values won't work if children aren't of uniform sign,
             // then again, this chart is not right to display those trees
             //boolean negate = total != null && total.signum() < 0;
         } else {
-            pieChart.setData(FXCollections.emptyObservableList());
-            pieChart.setTitle("No Data");
+            creditPieChart.setData(FXCollections.emptyObservableList());
+            creditPieChart.setTitle("No Data");
+
+            debitPieChart.setData(FXCollections.emptyObservableList());
+            debitPieChart.setTitle("No Data");
         }
     }
 
+
+    private ObservableList<PieChart.Data>[] createPieDataSet(@NotNull final Account account) {
+
+        @SuppressWarnings("unchecked")
+        final ObservableList<PieChart.Data>[] chartData = (ObservableList<PieChart.Data>[]) new ObservableList[2];
+
+        chartData[CREDIT] = FXCollections.observableArrayList();
+        chartData[DEBIT] = FXCollections.observableArrayList();
+
+        final Map<String, BigDecimal> names = new HashMap<>();
+
+
+        final List<TranTuple> list = getTransactions(account, new ArrayList<>(), startDatePicker.getValue(),
+                endDatePicker.getValue());
+
+        CurrencyNode currency = account.getCurrencyNode();
+
+        //final NumberFormat numberFormat = CommodityFormat.getFullNumberFormat(currency);
+
+        for (final TranTuple tranTuple : list) {
+
+            Transaction tran = tranTuple.transaction;
+            //Account account = tranTuple.account;
+
+            String payee = tran.getPayee();
+            BigDecimal sum = tran.getAmount(tranTuple.account);
+
+            sum = sum.multiply(tranTuple.account.getCurrencyNode().getExchangeRate(currency));
+
+            /*if (useFilters.isSelected()) {
+                for (String aFilterList : filterList) {
+
+                    PayeeMatcher pm = new PayeeMatcher(aFilterList, false);
+
+                    if (pm.matches(tran)) {
+                        payee = aFilterList;
+                        //System.out.println(filterList.get(i));
+                        break;
+                    }
+                }
+            }*/
+
+            if (names.containsKey(payee)) {
+                sum = sum.add(names.get(payee));
+            }
+
+            names.put(payee, sum);
+        }
+
+        for (final Map.Entry<String, BigDecimal> entry : names.entrySet()) {
+            //final String label = child.getName() + " - " + numberFormat.format(value);
+            final PieChart.Data data = new PieChart.Data(truncateName(entry.getKey()),
+                    entry.getValue().abs().doubleValue());
+
+            // nodes are created lazily.  Set the user data (Account) after the node is created
+            //data.nodeProperty().addListener((observable, oldValue, newValue) -> newValue.setUserData(child));
+
+            if (entry.getValue().signum() == -1) {
+                chartData[DEBIT].add(data);
+            } else {
+                chartData[CREDIT].add(data);
+            }
+        }
+
+        return chartData;
+    }
+
+    private static String truncateName(final String name) {
+        if (name.length() <= MAX_NAME_LENGTH) {
+            return name;
+        } else {
+            return name.substring(0, MAX_NAME_LENGTH - 1) + ELLIPSIS;
+        }
+    }
+
+    private static class TranTuple {
+
+        final Account account;
+
+        final Transaction transaction;
+
+        public TranTuple(Account account, Transaction transaction) {
+            this.account = account;
+            this.transaction = transaction;
+        }
+    }
+
+    private List<TranTuple> getTransactions(final Account account, final List<TranTuple> transactions,
+                                            final LocalDate startDate, final LocalDate endDate) {
+
+        for (final Transaction transaction : account.getTransactions(startDate, endDate)) {
+            TranTuple tuple = new TranTuple(account, transaction);
+            transactions.add(tuple);
+        }
+
+        for (final Account child : account.getChildren(Comparators.getAccountByCode())) {
+            getTransactions(child, transactions, startDate, endDate);
+        }
+
+        return transactions;
+    }
+
+
     @FXML
     private void handleSaveAction() {
-        ChartUtilities.saveChart(pieChart);
+        ChartUtilities.saveChart(creditPieChart);
     }
 
     @FXML
     private void handleCopyToClipboard() {
-        ChartUtilities.copyToClipboard(pieChart);
+        ChartUtilities.copyToClipboard(creditPieChart);
     }
 
     @FXML
     private void handlePrintAction() {
-        ChartUtilities.printChart(pieChart);
+        ChartUtilities.printChart(creditPieChart);
     }
 
     @FXML
