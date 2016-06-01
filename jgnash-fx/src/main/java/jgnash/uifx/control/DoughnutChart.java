146901f62692966386852ff7837146fedf4e4805
Craig Cavanaugh <jgnash.devel@gmail.com>
2016-06-01 05:30:08 -0400

    Add subtitle

--- jgnash-fx/src/main/java/jgnash/uifx/control/DoughnutChart.java
+++ jgnash-fx/src/main/java/jgnash/uifx/control/DoughnutChart.java
@@ -24,7 +24,10 @@ public class DoughnutChart extends PieChart {
 
     private final Text titleText;
 
+    private final Text subTitleText;
+
     private final StringProperty centerTitle = new SimpleStringProperty();
+    private final StringProperty centerSubTitle = new SimpleStringProperty();
 
     @SuppressWarnings("unused")
     public DoughnutChart() {
@@ -41,10 +44,19 @@ public class DoughnutChart extends PieChart {
         titleText.setStyle("-fx-font-size: 1.4em");
         titleText.setTextAlignment(TextAlignment.JUSTIFY);
         titleText.textProperty().bind(centerTitle);
+
+        subTitleText = new Text();
+        subTitleText.setStyle("-fx-font-size: 1.0em");
+        subTitleText.setTextAlignment(TextAlignment.JUSTIFY);
+        subTitleText.textProperty().bind(centerSubTitle);
     }
 
     public StringProperty centerTitleProperty() {
         return centerTitle;
+    }
+
+    public StringProperty centerSubTitleProperty() {
+        return centerSubTitle;
     }
 
     @Override
@@ -66,6 +78,7 @@ public class DoughnutChart extends PieChart {
                 if (!parent.getChildren().contains(hole)) {
                     parent.getChildren().add(hole);
                     parent.getChildren().add(titleText);
+                    parent.getChildren().add(subTitleText);
                 }
             }
         }
@@ -86,13 +99,16 @@ public class DoughnutChart extends PieChart {
         final double maxY = getData().parallelStream().mapToDouble(value -> value.getNode().getBoundsInParent()
                 .getMaxY()).max().orElse(Double.MAX_VALUE);
 
-        // center the hole and size
+        // center the hole and set radius
         hole.setCenterX(minX + (maxX - minX) / 2);
         hole.setCenterY(minY + (maxY - minY) / 2);
         hole.setRadius((maxX - minX) / RING_WIDTH);
 
-        // center the title
+        // center the title and subtitle
         titleText.setX((minX + (maxX - minX) / 2) - titleText.getLayoutBounds().getWidth() / 2);
         titleText.setY(minY + (maxY - minY) / 2);
+
+        subTitleText.setX((minX + (maxX - minX) / 2) - subTitleText.getLayoutBounds().getWidth() / 2);
+        subTitleText.setY((minY + (maxY - minY) / 2) + subTitleText.getLayoutBounds().getHeight());
     }
 }
