b06c1bfd386948ddcc078aea2129bacba287b6ce
Craig Cavanaugh <jgnash.devel@gmail.com>
2016-07-03 06:32:13 -0400

    Fix javadoc

--- jgnash-resources/src/main/java/jgnash/util/Version.java
+++ jgnash-resources/src/main/java/jgnash/util/Version.java
@@ -29,7 +29,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Utility class for application version
+ * Utility class for application version.
  *
  * @author Craig Cavanaugh
  */
