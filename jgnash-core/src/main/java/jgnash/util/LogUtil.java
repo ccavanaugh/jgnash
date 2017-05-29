eeb1e93f25ef9d084cbbd480f858638630801a11
Craig Cavanaugh <jgnash.devel@gmail.com>
2017-05-29 05:47:58 -0400

    new utility class

---
+++ jgnash-core/src/main/java/jgnash/util/LogUtil.java
@@ -0,0 +1,46 @@
+/*
+ * jGnash, a personal finance application
+ * Copyright (C) 2001-2017 Craig Cavanaugh
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jgnash.util;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Utility class to reduce logging code
+ *
+ * A fair amount of byte code is generated to log a throwable.
+ *
+ *  This class provides a static method which produces much less byte code when used.
+ *
+ * @author Craig Cavanaugh
+ */
+public class LogUtil {
+
+    private LogUtil() {
+        // utility class
+    }
+
+    /**
+     * Logs a throwable at Level.SEVERE.
+     * @param clazz calling class
+     * @param throwable Throwable to log
+     */
+    public static void logSevere(final Class clazz, final Throwable throwable) {
+        Logger.getLogger(clazz.getName()).log(Level.SEVERE, throwable.getLocalizedMessage(), throwable);
+    }
+}
