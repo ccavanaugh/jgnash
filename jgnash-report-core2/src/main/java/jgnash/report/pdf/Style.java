2a0c3a76b88edebb9af203f1b62e6421c8535f01
Craig Cavanaugh <jgnash.devel@gmail.com>
2018-09-22 06:38:41 -0400

    Playing around with OpenPDF

---
+++ jgnash-report-core2/src/main/java/jgnash/report/pdf/Style.java
@@ -0,0 +1,57 @@
+package jgnash.report.pdf;
+
+import com.lowagie.text.Element;
+import com.lowagie.text.pdf.PdfPCell;
+
+import java.awt.Color;
+
+public class Style {
+
+    public static void headerCellStyle(PdfPCell cell){
+
+        // alignment
+        cell.setHorizontalAlignment(Element.ALIGN_LEFT);
+        cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+
+        // padding
+        cell.setPaddingLeft(4);
+        cell.setPaddingRight(4);
+        cell.setPaddingBottom(2);
+
+        // background color
+        cell.setBackgroundColor(Color.LIGHT_GRAY);
+
+        // border
+        cell.setBorder(0);
+        cell.setBorderWidthBottom(1);
+        cell.setBorderColorBottom(Color.GRAY);
+
+        // height
+        cell.setMinimumHeight(18);
+
+    }
+
+    public static void valueCellStyle(PdfPCell cell){
+        // alignment
+        cell.setHorizontalAlignment(Element.ALIGN_LEFT);
+        cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+
+        // padding
+        cell.setPaddingLeft(4);
+        cell.setPaddingRight(4);
+
+        // border
+        cell.setBorder(0);
+        cell.setBorderWidthBottom(0.5f);
+
+        // height
+        cell.setMinimumHeight(18);
+    }
+
+    public static void numericValueCellStyle(PdfPCell cell){
+        valueCellStyle(cell);
+
+        // alignment
+        cell.setHorizontalAlignment(Element.ALIGN_RIGHT);
+    }
+}
