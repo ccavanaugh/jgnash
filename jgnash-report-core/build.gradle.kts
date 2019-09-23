aa3cda789f20c282d0fd885e50d75c022bb392d0
Craig Cavanaugh <jgnash.devel@gmail.com>
2019-09-23 04:41:55 -0400

    Convert to Kotlin

---
+++ jgnash-report-core/build.gradle.kts
@@ -0,0 +1,24 @@
+description = "jGnash Report Core"
+
+val moduleName = "jgnash.report"
+
+val apachePoiVersion: String by project
+val pdfBoxVersion: String by project
+val commonsLangVersion: String by project
+
+dependencies {
+    implementation(project(":jgnash-core"))
+
+    implementation("org.apache.poi:poi-ooxml:$apachePoiVersion") {
+        exclude(module = "stax-api")
+        exclude(module = "xml-apis")
+    }
+
+    implementation("org.apache.pdfbox:pdfbox:$pdfBoxVersion")
+    implementation("org.apache.pdfbox:pdfbox-tools:$pdfBoxVersion")
+    implementation("org.apache.commons:commons-lang3:$commonsLangVersion")
+}
+
+tasks.jar {
+    manifest.attributes["Automatic-Module-Name"] = moduleName
+}
