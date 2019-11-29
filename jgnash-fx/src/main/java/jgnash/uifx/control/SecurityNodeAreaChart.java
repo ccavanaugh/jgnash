5d7462f274f860720c7865ad4bbdddffa186c8ad
Craig Cavanaugh <jgnash.devel@gmail.com>
2019-11-29 10:38:33 -0500

    Prevent a NPE when changing the Symbol of a Security shortly after viewing it's chart.

--- jgnash-fx/src/main/java/jgnash/uifx/control/SecurityNodeAreaChart.java
+++ jgnash-fx/src/main/java/jgnash/uifx/control/SecurityNodeAreaChart.java
@@ -68,40 +68,41 @@ public class SecurityNodeAreaChart extends AreaChart<Number, Number> {
     }
 
     public void update() {
-
         JavaFXUtils.runLater(getData()::clear); // clear old data
 
         new Thread(() -> {
             final SecurityNode securityNode = securityNodeProperty().get();
-            final Optional<LocalDate[]> optional = securityNode.getLocalDateBounds();
 
-            optional.ifPresent(localDates -> {
-                final List<List<SecurityHistoryNode>> groups = securityNode.getHistoryNodeGroupsBySplits();
+            if (securityNode != null) { // protect against an NPE caused by security rename
+                final Optional<LocalDate[]> optional = securityNode.getLocalDateBounds();
 
-                for (int i = 0; i < groups.size(); i++) {
-                    final Series<Number, Number> series = new Series<>();
-                    series.setName(securityNode.getSymbol() + i);
+                optional.ifPresent(localDates -> {
+                    final List<List<SecurityHistoryNode>> groups = securityNode.getHistoryNodeGroupsBySplits();
 
-                    for (final SecurityHistoryNode node : groups.get(i)) {
-                        series.getData().add(new Data<>(node.getLocalDate().toEpochDay(), node.getAdjustedPrice()));
+                    for (int i = 0; i < groups.size(); i++) {
+                        final Series<Number, Number> series = new Series<>();
+                        series.setName(securityNode.getSymbol() + i);
+
+                        for (final SecurityHistoryNode node : groups.get(i)) {
+                            series.getData().add(new Data<>(node.getLocalDate().toEpochDay(), node.getAdjustedPrice()));
+                        }
+
+                        JavaFXUtils.runLater(() -> getData().add(series));
                     }
 
-                    JavaFXUtils.runLater(() -> getData().add(series));
-                }
+                    xAxis.setLowerBound(localDates[0].toEpochDay());
+                    xAxis.setUpperBound(localDates[1].toEpochDay());
 
-                xAxis.setLowerBound(localDates[0].toEpochDay());
-                xAxis.setUpperBound(localDates[1].toEpochDay());
+                    final long range = localDates[1].toEpochDay() - localDates[0].toEpochDay();
 
-                final long range = localDates[1].toEpochDay() - localDates[0].toEpochDay();
-
-                if (range > TICK_MARKS) {
-                    xAxis.setTickUnit((int)((localDates[1].toEpochDay() - localDates[0].toEpochDay()) / TICK_MARKS));
-                } else {
-                    xAxis.setTickUnit(range - 1);
-                }
-            });
+                    if (range > TICK_MARKS) {
+                        xAxis.setTickUnit((int)((localDates[1].toEpochDay() - localDates[0].toEpochDay()) / TICK_MARKS));
+                    } else {
+                        xAxis.setTickUnit(range - 1);
+                    }
+                });
+            }
         }).start();
-
     }
 
     private static class NumberDateStringConverter extends StringConverter<Number> {
