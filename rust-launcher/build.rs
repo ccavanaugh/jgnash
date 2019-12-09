fde8d28145189e64050bd4fae65358551d876dce
Craig Cavanaugh <jgnash.devel@gmail.com>
2019-12-09 05:55:49 -0500

    Fix build.rs for unix

--- rust-launcher/build.rs
+++ rust-launcher/build.rs
@@ -1,11 +1,18 @@
 // adds an icon to the windows executable
 
+#[cfg(target_family = "windows")]
 extern crate winres;
 
+#[cfg(target_family = "windows")]
 fn main() {
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
         res.set_icon("gnome-money.ico");
         res.compile().unwrap();
     }
+}
+
+#[cfg(target_family = "unix")]
+fn main() {
+    // empty for unix build
 }
