6e4e4ebdba0725ae08c758886ce590a5cc626372
Craig Cavanaugh <jgnash.devel@gmail.com>
2019-12-12 17:41:24 -0500

    Check for minimum java version

--- rust-launcher/src/main.rs
+++ rust-launcher/src/main.rs
@@ -4,6 +4,7 @@ extern crate java_locator;
 extern crate msgbox;
 
 use std::env;
+use std::f32;
 use std::ops::Add;
 use std::path::PathBuf;
 use std::process;
@@ -11,29 +12,38 @@ use std::process::Command;
 
 use msgbox::IconType;
 
-fn main() {
-    let java_home = java_locator::locate_java_home();
+const MIN_JAVA_VERSION: f32 = 11.0 - (2.0 * f32::EPSILON);
 
-    match java_home {
-        Ok(s) => launch_jgnash(s),
-        Err(_e) => msgbox::create(
+fn main() {
+    let v = get_java_version();
+
+    if v >= MIN_JAVA_VERSION {
+        let java_home = java_locator::locate_java_home();
+
+        match java_home {
+            Ok(_s) => launch_jgnash(),
+            Err(_e) => msgbox::create(
+                "Error",
+                "Unable to locate a valid Java installation.\n\n
+                 Please check your download a JVM from https://adoptopenjdk.net.",
+                IconType::Error,
+            ),
+        }
+    } else {
+        msgbox::create(
             "Error",
-            "Unable to locate a valid Java installation.\n\n\
+            "Your Java installation is too old or misconfigured.\n\n\
              Please download a JVM from https://adoptopenjdk.net.",
             IconType::Error,
-        ),
+        )
     }
 }
 
 #[cfg(target_family = "windows")]
-fn launch_jgnash(s: String) {
-
+fn launch_jgnash() {
     // collect environment variables; the fist is the path that launched the program
     let mut args: Vec<String> = env::args().collect();
     args.remove(0);
-
-    // the java executable
-    let java_exe = s.add("\\bin\\javaw.exe");
 
     //let class_path = "c:\\temp\\jGnash-3.4.0\\lib\\*";
 
@@ -44,7 +54,7 @@ fn launch_jgnash(s: String) {
         .to_string()
         .add("\\lib\\*");
 
-    let status = Command::new(&java_exe)
+    let status = Command::new(get_java_exe())
         .arg("-classpath")
         .arg(&class_path)
         .arg("jGnash")
@@ -56,14 +66,10 @@ fn launch_jgnash(s: String) {
 }
 
 #[cfg(target_family = "unix")]
-fn launch_jgnash(s: String) {
-
+fn launch_jgnash() {
     // collect environment variables; the fist is the path that launched the program
     let mut args: Vec<String> = env::args().collect();
     args.remove(0);
-
-    // the java executable
-    let java_exe = s.add("/bin/java");
 
     let class_path = get_execution_path()
         .as_os_str()
@@ -72,7 +78,7 @@ fn launch_jgnash(s: String) {
         .to_string()
         .add("/lib/*");
 
-    let status = Command::new(&java_exe)
+    let status = Command::new(get_java_exe())
         .arg("-classpath")
         .arg(&class_path)
         .arg("jGnash")
@@ -91,4 +97,66 @@ fn get_execution_path() -> PathBuf {
         }
         Err(_e) => PathBuf::new(),
     }
+}
+
+/// Executes java --version and extracts the version
+fn get_java_version() -> f32 {
+    let mut version = -1.0_f32;
+
+    let output = Command::new(get_java_exe())
+        .arg("--version")
+        .output()
+        .expect("failed to execute command");
+
+    let st = String::from_utf8(output.stdout).unwrap();
+
+    if !st.is_empty() {
+        // look for the first available number
+        let mut decimal_found = false;
+        let mut version_string = String::new();
+
+        // munch through the string one char at a time and extract the first decimal
+        for c in st.chars() {
+            if c.is_ascii_digit() {
+                version_string.push(c);
+            }
+
+            if c == '.' {
+                if decimal_found {
+                    // we don't want the tertiary decimal
+                    break;
+                }
+
+                version_string.push(c);
+                decimal_found = true;
+            }
+
+            // must have found a xx.xx decimal instead of xx.xx.x
+            if c.is_ascii_whitespace() && decimal_found {
+                break;
+            }
+        }
+
+        if !version_string.is_empty() {
+            version = match version_string.parse::<f32>() {
+                Ok(f) => f,
+                Err(_e) => {
+                    eprintln!("failed to parse: {}", version_string);
+                    version
+                }
+            }
+        }
+    }
+
+    return version;
+}
+
+#[cfg(target_family = "unix")]
+fn get_java_exe() -> String {
+    java_locator::locate_java_home().unwrap().add("/bin/java")
+}
+
+#[cfg(target_family = "windows")]
+fn get_java_exe() -> String {
+    java_locator::locate_java_home().unwrap().add("\\bin\\javaw.exe")
 }
